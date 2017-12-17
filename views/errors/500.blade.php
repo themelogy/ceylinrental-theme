776c96a83b04e7abb9edb46bbeb9f2528db8b11b
sagsoz06 <sagsoz06@gmail.com>
2017-12-17 14:28:34 +0300

    Fixed 500 page

--- views/errors/500.blade.php
+++ views/errors/500.blade.php
@@ -1,24 +1,26 @@
 @extends('layouts.master')
 
-@section('content-header')
-    <h1>
-        {{ trans('core::core.error 500') }}
-    </h1>
-    <ol class="breadcrumb">
-        <li><a href="{{ route('dashboard.index') }}"><i class="fa fa-dashboard"></i> {{ trans('core::core.breadcrumb.home') }}</a></li>
-        <li class="current">{{ trans('core::core.error 500') }}</li>
-    </ol>
-@stop
-
+@section('breadcrumbs')
+    <!-- BREADCRUMBS -->
+    <section class="page-section breadcrumbs text-center">
+        <div class="container">
+            <div class="page-header">
+                <h1>500</h1>
+            </div>
+            {!! Breadcrumbs::renderIfExists('page') !!}
+        </div>
+    </section>
+    <!-- /BREADCRUMBS -->
+@endsection
 
 @section('content')
-    <div class="error-page">
-        <h2 class="headline text-red" style="line-height: 0.6; margin-top: 0;"> 500</h2>
-
-        <div class="error-content">
-            <h3><i class="fa fa-warning text-red"></i> {{ trans('core::core.error 500 title') }}</h3>
+    <!-- PAGE -->
+    <section class="page-section text-center error-page light">
+        <div class="container">
+            <h3>500</h3>
+            <h2><i class="fa fa-warning"></i> {{ trans('core::core.error 500 title') }}</h2>
             <p>{!! trans('core::core.error 500 description') !!}</p>
         </div>
-        <!-- /.error-content -->
-    </div>
+    </section>
+    <!-- /PAGE -->
 @stop
